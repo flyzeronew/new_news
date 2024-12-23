364c269cc1e9f6bb5570e45555521ceaf551e58f
flyzero <flyzeronew@gmail.com>
2024-12-23 15:29:29 +0800

    修改

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = (props) => {
       <main className={styles.homePage}>
         <div className='frameBox'>
           <article>
-              主欄標示分支1
+              主欄標示分支12222
           </article>
           <aside>
               右欄
