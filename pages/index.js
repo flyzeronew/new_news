e3d86cbd70942e395914eb0fa929b8eb8bbe0fac
flyzeronew <flyzeronew@gmail.com>
2024-12-24 13:44:33 +0800

    移除所有文字

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = (props) => {
       <main className={styles.homePage}>
         <div className='frameBox'>
           <article>
-              主欄1224練習222333
+              主欄
           </article>
           <aside>
               右欄
