34e502c49ae9ce06f59a7ce4149d3706d8d5fba6
flyzeronew <flyzeronew@gmail.com>
2024-12-24 13:55:47 +0800

    新增3

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = (props) => {
       <main className={styles.homePage}>
         <div className='frameBox'>
           <article>
-              主欄12
+              主欄123
           </article>
           <aside>
               右欄
