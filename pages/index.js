10227aa5d71bbe216dc9171bdc133d61666122ce
flyzero <flyzeronew@tvbs.com.tw>
2025-02-04 11:08:45 +0800

    fix:主欄新增2
    (cherry picked from commit 2d710d4447c580e7c97105c3e22f2bdaced7111f)
    (cherry picked from commit f62690b4167e8f637042f345f3959a06b6b7ab79)

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = (props) => {
       <main className={styles.homePage}>
         <div className='frameBox'>
           <article>
-              主欄1
+              主欄12
           </article>
           <aside>
               右欄2
