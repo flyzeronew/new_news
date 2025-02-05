9a0dc32b1d86cf0f12a7a5dee7ce204062513a40
flyzero <flyzeronew@tvbs.com.tw>
2025-02-05 11:31:43 +0800

    fix:修正Script

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,38 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
 export default function Document() {
   const gtmId = process.env.GTM_ID;
+
   return (
-    <Html lang='zh-Hant-TW'>
-      <Head />        
-        {/* GTM */}
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `    
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-            `,
-          }}
-        />
+    <Html lang="zh-Hant-TW">
+      <Head>
+        {/* 使用 next/script 來載入 jQuery */}
+        <Script src="https://code.jquery.com/jquery-3.6.0.min.js" strategy="beforeInteractive" />
+
+        {/* 使用 next/script 來載入 GTM */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `}
+        </Script>
+      </Head>
       <body>
+        {/* GTM noscript 放在 body 內 */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Main />
         <NextScript />
-        {/* GTM */}
-        <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-        </noscript>
       </body>
     </Html>
   );
